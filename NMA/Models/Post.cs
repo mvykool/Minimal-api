e17666cdf0659cb77937536e22a6d2f785d5ab83
maicol hernandez <maykolandres4@gmail.com>
2023-06-04 03:58:48 -0500

    added models to Domain

---
+++ NMA/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Post       //change to public touse it in other layers
+    {
+        public int Id { get; set; }
+
+        public string? Content { get; set; }   //the ? mark makes it nullable
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime LastModified { get; set; }
+    }
+}
