a1348d1e910e949bf997f43815e78b81a52f2576
maicol hernandez <maykolandres4@gmail.com>
2023-06-05 01:27:08 -0500

    few changes and installed few things

---
+++ DataAcess/Migrations/20230605052348_Init.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAcess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,5 +1,16 @@
+using DataAcess;
+using Microsoft.EntityFrameworkCore;
+using Application.Abstractions;
+using DataAcess.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
+//connect to database
+var cs = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<SocialDbContext>(opt => opt.UseSqlServer(cs));
+
+builder.Services.AddScoped<IPostRepository,PostRepository>();
 
 var app = builder.Build();
 
